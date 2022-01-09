e6878d98b563c640647e3db9cb4106738db8bc9e
mszgajewski <mszgajewski@gmail.com>
2022-01-10 00:01:48 +0100

    Initial Commit

--- src/main/resources/database/2020-09-29/02-create-comment.sql
+++ src/main/resources/database/2020-09-29/02-create-comment.sql
@@ -6,7 +6,7 @@ CREATE TABLE COMMENT (
     content VARCHAR(2000) NULL,
     created timestamp
 );
-
+--hangeset mgajewski:3
 ALTER TABLE COMMENT
     ADD CONSTRAINT comment_post_id
     FOREIGN KEY (post_id) REFERENCES post(id)
